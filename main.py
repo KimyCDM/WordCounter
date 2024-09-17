4c5107be04b1a64a12f0445766862df2268080c6
KimyCDM <kimy@educ.college-montreal.qc.ca>
2024-09-17 15:37:47 -0400

    final

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-'''Compteur de mot par Yul Kim 405'''
+'''
+Compteur de mot
+Par Yul Kim
+Groupe:405
+Un compteur de mot
+'''
 def count_word(phrase):
     print('Il y a',len(phrase.split(" ")),'mot(s)')
 phrase=input('Quel est votre phrase?:')
